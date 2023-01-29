bff4e5ba0d9a7a04fef862cc56b38e0bad77ccdc
SergeyKartavcev <102188021+SergeyKartavcev@users.noreply.github.com>
2023-01-29 10:17:49 +0200

    fix test

--- test/integretion/register.test.js
+++ test/integretion/register.test.js
@@ -7,22 +7,40 @@ const mongoose = require("mongoose");
 mongoose.set("strictQuery", false);
 
 const { HOST_TEST_URI } = process.env;
+
 describe("register", () => {
   beforeAll(async () => {
     await mongoose.connect(HOST_TEST_URI);
 
     await User.deleteMany();
   });
+
   afterAll(async () => {
     await mongoose.disconnect(HOST_TEST_URI);
   });
-  it("should register new user", async () => {
-   const response = await supertest(app).post("/api/auth/register").send({
-    email: "testUser@mail.com",
-    password: "12345"
-   })
 
-   expect(response.statusCode).toBe(201);
-   expect(response.body.data.user.email).toBe("testUser@mail.com");
+  it("should register new user", async () => {
+    const response = await supertest(app).post("/api/auth/register").send({
+      email: "testUser1@gmail.com",
+      password: "123456",
+    });
+
+    expect(response.statusCode).toBe(201);
+    expect(response.body.data.user.email).toBe("testUser1@gmail.com");
   });
+
+  it("should not register the same user 2 times", async () => {
+    await supertest(app).post("/api/auth/register").send({
+      email: "testUser2@gmail.com",
+      password: "123456",
+    });
+
+    const response = await supertest(app).post("/api/auth/register").send({
+      email: "testUser2@gmail.com",
+      password: "123456",
+    });
+    //
+    expect(response.statusCode).toBe(500);
+  });
+ 
 });
